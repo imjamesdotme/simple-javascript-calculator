f9657a3de35e1cbf7933d1cf59abc49711154d13
James L <hello@imjames.me>
2016-02-01 20:10:31 +0000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Simple Javascript Calculator
+
+Simple responsive javascript calculator. Allows for simple calculations & also features a 'tape' function allowing you to see previous calculations made.
+
+Built for the Free Code Camp project [Build a Random Quote Machine](http://http://www.freecodecamp.com/challenges/build-a-javascript-calculator).
+
+Created with the jQuery & Bourbon.
+
+View the final project [here](http://projects.imjames.me/simple-javascript-calculator/).
---
+++ src/calculator.js
@@ -0,0 +1,168 @@
+$(document).ready(function () {
+
+  var num1;
+  var num2;
+  var operator;
+  var operatorText;
+  var total;
+
+  // Used for multiple numbers to be inputted on the display & written to one of the num variables. Each time a numeric button is pushed its added into the array, the array is then joined & outputted to the display.
+  var displayNumbers = [];
+  var tape = [];
+
+  $('#keys button.number, #decimal').on('click', function() {
+
+    var getPressedNumber = $(this).text();
+    displayNumbers.push(getPressedNumber);
+
+    $('#display span').text(displayNumbers.join(''));
+
+  });
+
+  $('#keys button.operator').on('click', function() {
+
+    // Store the number currently in the display and parse the string to a number.
+    num1 = $('#display span').text();
+    num1 = parseFloat(num1);
+
+    // Clear display - the
+    displayNumbers = [];
+
+    operator = $(this).text();
+
+    // Display operator symbol as text on the display
+    switch(operator) {
+      case "+":
+        operatorText = 'Plus';
+        break;
+
+      case "-":
+        operatorText = 'Minus';
+        break;
+
+      case "*":
+        operatorText = 'Times';
+        break;
+
+      case "/":
+        operatorText = 'Divide';
+        break;
+    }
+
+    $('#display span').text(operatorText);
+
+  });
+
+  // Equals
+  $('button#equals').on('click', function() {
+
+    if(num1 === 0 && num2 === 0) {
+      $('#display span').text('0');
+    }
+
+    num2 = $('#display span').text();
+    num2 = parseFloat(num2);
+
+    switch(operator) {
+      case "+":
+        total = add(num1, num2);
+        break;
+
+      case "-":
+        total = subtract(num1, num2);
+        break;
+
+      case "*":
+        total = multiply(num1, num2);
+        break;
+
+      case "/":
+        total = divide(num1, num2);
+        break;
+
+    }
+
+    $('#display span').text(total);
+
+    var equals = '=';
+
+    // Create a tape entry
+    tape.push(num1 + operator + num2 + equals + total);
+    tapeDisplay();
+
+  });
+
+  // Clear all
+  $('#clear').on('click', function() {
+
+    num1 = null;
+    num2 = null;
+    operator = null;
+    total = null;
+    displayNumbers = [];
+
+    $('#display span').text('0');
+
+  });
+
+  // Main logic for calculations
+  function add(n1, n2) {
+    return n1 + n2;
+  }
+
+  function subtract(n1, n2) {
+    return n1 - n2;
+  }
+
+  function multiply(n1, n2) {
+    return n1 * n2;
+  }
+
+  function divide(n1, n2) {
+    return n1 / n2;
+  }
+
+  // Tape display
+  function tapeDisplay() {
+
+    // Clear display
+    $('#tape-list').empty('li');
+
+    if(tape.length) {
+      $('#tape-content span').text('Your calculations.');
+    } else {
+      $('#tape-content span').text('No calculations have been made.');
+    }
+
+    for(var i = 0; i < tape.length; i++) {
+
+      $('#tape-list').append('<li>' + tape[i].toString(tape[i]) + '</li>');
+
+    }
+  }
+
+  // Show/hide tape display
+  $('#tape').on('click', function() {
+
+    if($(this).hasClass('active')) {
+      $(this).removeClass('active');
+    } else {
+      $(this).addClass('active');
+    }
+
+    $('#tape-content').slideToggle();
+
+  });
+
+  // Clear tape
+  $('#clear-tape').on('click',function() {
+
+    $('#tape-list').empty('li');
+    tape = [];
+
+    // Run tapeDisplay to update calculations messaging.
+    tapeDisplay();
+
+  });
+
+});
